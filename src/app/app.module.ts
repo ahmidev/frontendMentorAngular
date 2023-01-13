30d47c76730296316d93b89dac631e554e001242
ahmidev <ahmid.aitouali@gmail>
2023-01-13 21:41:19 +0100

    fished style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { SelectPlanComponent } from './select-plan/select-plan.component';
+import { PickAddonsComponent } from './pick-addons/pick-addons.component';
+import { FinishingUpComponent } from './finishing-up/finishing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PersonalInfoComponent,
+    SelectPlanComponent,
+    PickAddonsComponent,
+    FinishingUpComponent
   ],
   imports: [
     BrowserModule,
